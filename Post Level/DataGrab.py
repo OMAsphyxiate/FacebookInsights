58394546b75d1a72997329bf6354a8a128fc0f0a
OMAsphyxiate <OMAsphyxiate@gmail.com>
2017-08-10 22:10:18 -0500

    Updated File

--- Post Level/DataGrab.py
+++ Post Level/DataGrab.py
@@ -2,33 +2,34 @@ import requests, facebook, urllib3, pprint, json, csv, io, os
 
 CSVFile = "OKCSouthDDS_Lifetime_Post_Consumer.csv"
 
-access_token = "EAACEdEose0cBAAMViesknnPGUXZBdbP64iOR2kZAbrNkjq6ne43rQNiTTZBCCANXZAmDkc5h6FcYZBWKsxd1H2Q9pgtE1ExcoAWguoCDDpNA2t5NKZA6cboWTwZCDgBdeUJKpaaS5xUf61jCZBEvfvPYqM79KndzTOaF882nIQZBnGEQJeju3GP9N4CZCBXptWtaQZD"
+access_token = ""
 
-
-postData = requests.get("https://graph.facebook.com/v2.10/OKCSouthDDS/posts?access_token="+access_token)
-
+postData = requests.get("https://graph.facebook.com/v2.10/OKCSouthDDS/posts?access_token=" + access_token)
 
 url = 'https://graph.facebook.com/v2.10/OKCSouthDDS/posts?fields='
 
-csv_fields = ["ClinicID","id","permalink_url","message","type","created_time","targeting","video play","other clicks","photo view","link clicks"]
-postFields = ('id,permalink_url,message,type,created_time,targeting,insights.metric(post_consumptions_by_type_unique).period(lifetime)')
+csv_fields = ["ClinicID", "id", "permalink_url", "message", "type", "created_time", "targeting", "video play",
+              "other clicks", "photo view", "link clicks"]
+postFields = (
+'id,permalink_url,message,type,created_time,targeting,insights.metric(post_consumptions_by_type_unique).period(lifetime)')
 
-request_post = requests.get(url+postFields+"&access_token="+access_token) #Sent REST request to GET data
+request_post = requests.get(url + postFields + "&access_token=" + access_token)  # Sent REST request to GET data
 
 try:
-	os.remove('OKCSouthDDS_Lifetime_Post_Consumer.csv') #Remove CSV file if already exists
+    os.remove('OKCSouthDDS_Lifetime_Post_Consumer.csv')  # Remove CSV file if already exists
 except OSError:
-	pass
+    pass
 
-json_data = json.loads(request_post.text) #Load JSON string into dictionary
+json_data = json.loads(request_post.text)  # Load JSON string into dictionary
 
-print (json_data['data']['id'])
-print (json_data['permalink_url'])
-print (json_data['message'])
-print (json_data['type'])
-print (json_data['created_time'])
-print (json_data['targeting'])
-print (json_data['insights']['video play'])
-print (json_data['insights']['other clicks'])
-print (json_data['insights']['photo view'])
-print (json_data['insights']['link clicks'])
+
+print(json_data['data'][0]['id']) #Print ID of first data row
+print(json_data['data'][0]['permalink_url']) #Print permalink of first data row
+print(json_data['data'][0]['message']) #Print message of first data row
+print(json_data['data'][0]['type']) #Print type of first data row
+print(json_data['data'][0]['created_time']) #Print created time of first data row
+#print(json_data['data'][0]['targeting']) #Print targeting for first data row
+print(json_data['data'][0]['insights']['data'][0]['values'][0]['value']['video play']) #Print Video Play of first data
+print(json_data['data'][0]['insights']['data'][0]['values'][0]['value']['other clicks']) #Print other clicks of first data
+print(json_data['data'][0]['insights']['data'][0]['values'][0]['value']['photo view']) #Print photo views of first data
+print(json_data['data'][0]['insights']['data'][0]['values'][0]['value']['link clicks']) #Print link clicks of first data
