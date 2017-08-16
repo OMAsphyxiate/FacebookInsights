30d4816c66b7ae8d7dfa25b99e711ba27a3ffe00
OMAsphyxiate <OMAsphyxiate@gmail.com>
2017-08-15 19:37:05 -0500

    And another

---
+++ Raw/NegativeFeedbackByType.py
@@ -0,0 +1,53 @@
+import csv, facebook, datetime, os
+
+access_token = 'EAACEdEose0cBAM3vwM4kcYcSV2mWSlobfqW9Ka09cYqigkVxZBNUO2e4HYhVzWa6Fv6I7HynrMFTNt2Vc1jZBxeh7xBBKHgnoZBvANgGZCmxyFBAtmY6jtXgu9ddNNZB1NUZBpZCPZBnbB4wtqaa1peglNgokrZCnLYy3ezZAWYPKhv4q1K3rSZBBSjuJVykqc5PyFudfPi3FP8KwZDZD'
+user = '159442580756185' #Page ID
+
+def PrintValues(*args): #Testing API data in print console
+    print(args)
+
+def WriteFile(filename,*args): #Write file in network storage
+    FilePath = "//10.10.10.252/datafiles/Dashboard/Facebook Data/%s.txt" %filename
+    if os.path.exists(FilePath): #Append if file Exists
+        append_check = 'a'
+    else: #Else create file
+        append_check = 'w'
+    with open(FilePath, append_check) as csvfile: #Write rows to file
+        writer = csv.writer(csvfile, delimiter='|')
+        writer.writerow(args)
+
+daterange = datetime.datetime.now() - datetime.timedelta(days=30)
+graph = facebook.GraphAPI(access_token)
+profile = graph.get_object(user)
+posts = graph.get_connections(profile['id'], 'insights/page_positive_feedback_by_type_unique?since=%s' %daterange)
+
+for post in posts['data']:
+    var1 = post['name']
+    var2 = post['period']
+    var3 = post['title']
+    var4 = post['description']
+    var5 = post['id']
+    for value in post['values']:
+        try:
+            var20 = value['value']['hide_all_clicks']
+        except:
+            var20 = 0
+        try:
+            var21 = value['value']['hide_clicks']
+        except:
+            var21 = 0
+        try:
+            var22 = value['value']['unlike_page_clicks']
+        except:
+            var22 = 0
+        try:
+            var23 = value['value']['report_spam_clicks']
+        except:
+            var23 = 0
+        try:
+            var24 = value['value']['xbutton_clicks']
+        except:
+            var24 = 0
+        var25 = value['end_time']
+        WriteFile('NegativeFeedbackByTypeUnique', user, var1,var2,var3,var4,var5,var20,var21,var22,var23,var24,var25)
+        #PrintValues(user,var1,var2,var3,var4,var5,var20,var21,var22,var23,var24,var25,var26,var27,var28)
